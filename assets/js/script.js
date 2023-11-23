e510532bdfd837dfef2845bf7c7d250f49d6d136
Emma Olson <emma.olson794@gmail.com>
2023-11-22 18:48:10 -0600

    beginning init, startGame, and checkClickTarget functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,23 @@
-console.log("hello tictac")
+const startBtn = document.getElementById("startbtn");
+const gameDiv = document.getElementById("grid");
+// declare variable for who's turn it is
+let turn = true;
+
+function checkClickTarget(event) {
+    let space = event.target.getAttribute("data-blank");
+    return space;
+};
+
+function startGame() {
+    gameDiv.addEventListener("click", checkClickTarget);
+    // toggle back and forth between whos turn it is
+};
+
+function init() {
+    console.log("init function")
+    // clear out previous game
+    // start new game
+    startGame();
+};
+
+startBtn.addEventListener("click", init);
