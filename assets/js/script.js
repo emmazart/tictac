3987bb5da6ee13aab92feb44ff2100d3a7277e7b
Emma Olson <emma.olson794@gmail.com>
2023-12-11 19:15:31 -0600

    added turn taking functionality bewteen user and computer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,37 @@
 const startBtn = document.getElementById("startbtn");
 const gameDiv = document.getElementById("grid");
+const player = document.getElementById("player");
+const computer = document.getElementById("computer");
+
 // declare variable for who's turn it is
-let turn = true;
+// true = player / false = computer
+let turn;
 
 function checkClickTarget(event) {
-    let space = event.target.getAttribute("data-blank");
-    return space;
+    let space = event.target;
+    turn ? space.innerHTML = "<p>X</p<" : space.innerHTML = "<p>O</p<";
+    return turn = !turn; 
+
+    // check all spaces have been used
+    // check win game (3 in a row)
+    // clear out spaces and start over
+};
+
+function checkTurn(turn) {
+    if (turn) { 
+        player.setAttribute("id", "turn")
+        computer.removeAttribute("id", "turn")
+    } else {
+        computer.setAttribute("id", "turn")
+        player.removeAttribute("id", "turn")
+    };
 };
 
 function startGame() {
+    // make it the player's turn
+    turn = true;
+    checkTurn(turn);
     gameDiv.addEventListener("click", checkClickTarget);
-    // toggle back and forth between whos turn it is
 };
 
 function init() {
