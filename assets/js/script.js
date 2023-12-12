fe64edd28fc7511383e593962ba0579a9f7d259d
Emma Olson <emma.olson794@gmail.com>
2023-12-11 21:09:21 -0600

    added beginning of checkWin function

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,20 +2,13 @@ const startBtn = document.getElementById("startbtn");
 const gameDiv = document.getElementById("grid");
 const player = document.getElementById("player");
 const computer = document.getElementById("computer");
+let spacesLeft = 9;
+let win = false;
+
 
 // declare variable for who's turn it is
 // true = player / false = computer
 let turn;
-
-function checkClickTarget(event) {
-    let space = event.target;
-    turn ? space.innerHTML = "<p>X</p<" : space.innerHTML = "<p>O</p<";
-    return turn = !turn; 
-
-    // check all spaces have been used
-    // check win game (3 in a row)
-    // clear out spaces and start over
-};
 
 function checkTurn(turn) {
     if (turn) { 
@@ -25,6 +18,65 @@ function checkTurn(turn) {
         computer.setAttribute("id", "turn")
         player.removeAttribute("id", "turn")
     };
+};
+
+function checkWin() {
+    const columns = document.getElementsByClassName("column");
+    let columnsArr = [];
+
+    // turn columns into an array so we can map it
+    for (const col of columns) {
+        columnsArr.push(col);
+    };
+
+    const spaces = columnsArr.map((col) => col.children[0].innerText);
+    console.log(spaces);
+
+    // [ 0, 3, 6]
+    // [ 1, 4, 7]
+    // [ 2, 5, 8]
+
+    // check all spaces have been used
+    // check win game (3 in a row)
+    // let token = ["X", "O"];
+    // for (let x = 0; x < token.length; x++){
+    //     if (spaces[0] === token[x] & spaces[1] === token[x] & spaces[2] === token[x]){
+    //         x = 0 ? win = true : win = false;
+    //     } else if (spaces[0] === token[x] & spaces[3] === token[x] & spaces[6] === token[x]) {
+    //         x = 0 ? win = true : win = false;
+    //     } else if (spaces[0] === token[x] & spaces[4] === token[x] & spaces[8] === token[x]) {
+    //         x = 0 ? win = true : win = false;
+    //     }  else if (spaces[8] === token[x] & spaces[5] === token[x] & spaces[2] === token[x]) {
+    //         x = 0 ? win = true : win = false;
+    //     } else if (spaces[8] === token[x] & spaces[7] === token[x] & spaces[6] === token[x]) {
+    //         x = 0 ? win = true : win = false;
+    //     }  else if (spaces[3] === token[x] & spaces[4] === token[x] & spaces[5] === token[x]) {
+    //         x = 0 ? win = true : win = false;
+    //     } else if (spaces[1] === token[x] & spaces[4] === token[x] & spaces[7] === token[x]) {
+    //         x = 0 ? win = true : win = false;
+    //     } else if (spaces[2] === token[x] & spaces[4] === token[x] & spaces[6] === token[x]) {
+    //         x = 0 ? win = true : win = false;
+    //     } else {
+    //       return;
+    //     }
+    // }
+
+    
+    // clear out spaces and start over
+}
+
+function checkClickTarget(event) {
+    let space = event.target;
+    turn ? space.innerHTML = "<p>X</p<" : space.innerHTML = "<p>O</p<";
+    turn = !turn; 
+    spacesLeft--;
+
+    if (spacesLeft <=4){
+        checkWin();
+    };
+
+    checkTurn(turn);
+
 };
 
 function startGame() {
